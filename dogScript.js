8551aae81f22bd44ac297ea2bf08d8e73dbd6b3e
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2022-08-04 20:40:20 +0200

    Added dogs version.

---
+++ dogScript.js
@@ -0,0 +1,4 @@
+function getMyDog(http_status){
+    document.getElementById('dogImage').src = '//http.dog/' + http_status + '.jpg';
+}
+//document.getElementById('catImage').src = '//http.cat/200';
